374474c0ffb02700d3a91f187854b10614b0414c
Alexandr <korchak.alexandr@gmail.com>
2020-01-04 00:06:32 +0200

    Добавление ссылок и адаптация под различные экраны

--- src/App.vue
+++ src/App.vue
@@ -2,8 +2,28 @@
   <!-- App.vue -->
 
   <v-app>
-    <v-navigation-drawer app temporary>
-      <!-- -->
+    <v-navigation-drawer
+      app
+      temporary
+      v-model="drawer"
+    >
+<!--TODO: сделать чтобы кнопка в меню дровера при выборе не была постоянно затененная      -->
+      <v-list>
+        <v-list-item-group color="primary">
+          <v-list-item
+            v-for="(link, i) of links"
+            :key="i"
+            :to="link.url"
+          >
+            <v-list-item-icon>
+              <v-icon>{{ link.icon }}</v-icon>
+            </v-list-item-icon>
+            <v-list-item-content>
+              <v-list-item-title v-text="link.title"></v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list-item-group>
+      </v-list>
     </v-navigation-drawer>
 
     <v-app-bar
@@ -11,14 +31,25 @@
       dark
       color="primary"
     >
-      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+      <v-app-bar-nav-icon
+        class="hidden-md-and-up"
+        @click="drawer = !drawer"
+      ></v-app-bar-nav-icon>
 
       <v-toolbar-title>Ad application</v-toolbar-title>
 
-      <v-spacer></v-spacer>
+      <v-spacer/>
 
       <v-toolbar-items class="hidden-sm-and-down">
-        <v-btn text>LINK ONE</v-btn>
+        <v-btn
+          v-for="link in links"
+          :key="link.title"
+          :to="link.url"
+          text
+        >
+          <v-icon left>{{ link.icon }}</v-icon>
+          {{ link.title }}
+        </v-btn>
       </v-toolbar-items>
 
     </v-app-bar>
@@ -33,5 +64,17 @@
 
 <script>
 export default {
+  data () {
+    return {
+      drawer: false,
+      links: [
+        { title: 'Login', icon: 'mdi-lock', url: '/login' },
+        { title: 'Registration', icon: 'mdi-face', url: '/registration' },
+        { title: 'Orders', icon: 'mdi-bookmark-outline', url: '/orders' },
+        { title: 'New ad', icon: 'mdi-file-plus', url: '/new' },
+        { title: 'My ads', icon: 'mdi-format-list-bulleted', url: '/list' }
+      ]
+    }
+  }
 }
 </script>
