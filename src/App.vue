b1e5110e2d9aac25989be3ffcb8473d74a6ad4de
Alexandr <korchak.alexandr@gmail.com>
2020-01-03 22:14:30 +0200

    Создание тулбара

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,34 @@
 <template>
-  <h1>Hello Vue</h1>
+  <!-- App.vue -->
+
+  <v-app>
+    <v-navigation-drawer app temporary>
+      <!-- -->
+    </v-navigation-drawer>
+
+    <v-app-bar
+      app
+      dark
+      color="primary"
+    >
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+
+      <v-toolbar-title>Ad application</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+      <v-toolbar-items class="hidden-sm-and-down">
+        <v-btn text>LINK ONE</v-btn>
+      </v-toolbar-items>
+
+    </v-app-bar>
+
+    <v-content>
+
+        <router-view></router-view>
+
+    </v-content>
+  </v-app>
 </template>
 
 <script>
