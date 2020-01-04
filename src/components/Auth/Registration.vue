01b26cd17b71b8c66f1836a6aefbbce50f68cda0
Alexandr <korchak.alexandr@gmail.com>
2020-01-04 18:05:26 +0200

    Создание страницы регистрации

--- src/components/Auth/Registration.vue
+++ src/components/Auth/Registration.vue
@@ -1,8 +1,68 @@
 <template>
-  <v-container>
-    <v-row>
-      <v-col sm="12">
-        <h1>Registration</h1>
+  <v-container
+    class="fill-height"
+    fluid
+  >
+    <v-row
+      align="center"
+      justify="center"
+    >
+      <v-col
+        cols="12"
+        sm="8"
+        md="6"
+      >
+        <v-card class="elevation-12">
+          <v-toolbar
+            color="primary"
+            dark
+            flat
+          >
+            <v-toolbar-title>Login form</v-toolbar-title>
+          </v-toolbar>
+          <v-card-text>
+            <v-form v-model="valid" ref="form">
+              <v-text-field
+                label="Email"
+                name="email"
+                prepend-icon="mdi-account"
+                type="email"
+                v-model="email"
+                :rules="emailRules"
+                validate-on-blur
+              />
+
+              <v-text-field
+                label="Password"
+                name="password"
+                prepend-icon="mdi-lock"
+                type="password"
+                :counter="20"
+                v-model="password"
+                :rules="passwordRules"
+                validate-on-blur
+              />
+
+              <v-text-field
+                label="Confirm Password"
+                name="confirm-password"
+                prepend-icon="mdi-lock"
+                type="password"
+                :counter="20"
+                v-model="confirmPassword"
+                :rules="confirmPasswordRules"
+              />
+            </v-form>
+          </v-card-text>
+          <v-card-actions>
+            <v-spacer />
+            <v-btn
+              color="primary"
+              @click="onSubmit"
+              :disabled="!valid"
+            >Create account!</v-btn>
+          </v-card-actions>
+        </v-card>
       </v-col>
     </v-row>
   </v-container>
@@ -12,6 +72,34 @@
 export default {
   data () {
     return {
+      email: '',
+      password: '',
+      confirmPassword: '',
+      valid: false,
+      emailRules: [
+        v => !!v || 'E-mail is required',
+        v => /.+@.+\..+/.test(v) || 'E-mail must be valid'
+      ],
+      passwordRules: [
+        v => !!v || 'Password is required',
+        v => (v && v.length >= 6) || 'Password must be equal or more than 6 characters'
+      ],
+      confirmPasswordRules: [
+        v => !!v || 'Password is required',
+        v => v === this.password || 'Password should match'
+      ]
+    }
+  },
+  methods: {
+    onSubmit () {
+      if (this.$refs.form.validate()) {
+        const user = {
+          email: this.email,
+          password: this.password
+        }
+        console.log(user)
+        console.log(this.$refs.form.value)
+      }
     }
   }
 }
