6f7d87e6dd139d456ada4dd0298c5ef6223be482
Alexandr <korchak.alexandr@gmail.com>
2020-01-04 11:45:55 +0200

    Регистрация роутов

---
+++ src/components/Ads/AdList.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-container>
+    <v-row>
+      <v-col sm="12">
+        <h1>AdList</h1>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/Auth/Registration.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-container>
+    <v-row>
+      <v-col sm="12">
+        <h1>Registration</h1>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
