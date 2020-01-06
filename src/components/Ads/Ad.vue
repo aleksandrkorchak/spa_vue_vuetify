82102282c5e989a83412e1be1fa5b35b3b86b485
Alexandr <korchak.alexandr@gmail.com>
2020-01-06 12:04:06 +0200

    Создание страницы объявления

--- src/components/Ads/Ad.vue
+++ src/components/Ads/Ad.vue
@@ -2,7 +2,21 @@
   <v-container>
     <v-row>
       <v-col sm="12">
-        <h1>Ad</h1>
+        <v-card>
+          <v-img
+            src="https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg"
+            height="300px"
+          ></v-img>
+          <v-card-text>
+            <h1 class="text--primary">lorem</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius, quam.</p>
+          </v-card-text>
+          <v-card-actions>
+            <v-spacer/>
+            <v-btn class="warning" depressed>Edit</v-btn>
+            <v-btn class="success">Buy</v-btn>
+          </v-card-actions>
+        </v-card>
       </v-col>
     </v-row>
   </v-container>
@@ -12,6 +26,15 @@
 export default {
   data () {
     return {
+      ads: [
+        {
+          title: 'First ad',
+          description: 'Hello I am description',
+          promo: false,
+          imageSrc: 'https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg',
+          id: '123'
+        }
+      ]
     }
   }
 }
