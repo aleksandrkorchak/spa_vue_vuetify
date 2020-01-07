ef49f96c9913785fe52f3f99b7f708b50904bd7d
Alexandr <korchak.alexandr@gmail.com>
2020-01-07 13:23:43 +0200

    Поддержание сессии пользователя

--- src/App.vue
+++ src/App.vue
@@ -19,9 +19,22 @@
               <v-icon>{{ link.icon }}</v-icon>
             </v-list-item-icon>
             <v-list-item-content>
-              <v-list-item-title v-text="link.title"></v-list-item-title>
+              <v-list-item-title v-text="link.title"/>
             </v-list-item-content>
           </v-list-item>
+
+          <v-list-item
+            v-if="isUserLoggedIn"
+            @click="onLogout"
+          >
+            <v-list-item-icon>
+              <v-icon>mdi-exit-to-app</v-icon>
+            </v-list-item-icon>
+            <v-list-item-content>
+              <v-list-item-title v-text="'Logout'"/>
+            </v-list-item-content>
+          </v-list-item>
+
         </v-list-item-group>
       </v-list>
     </v-navigation-drawer>
@@ -34,7 +47,7 @@
       <v-app-bar-nav-icon
         class="hidden-md-and-up"
         @click="drawer = !drawer"
-      ></v-app-bar-nav-icon>
+      />
 
       <v-toolbar-title>
         <router-link to="/" tag="span" class="pointer">Ad application</router-link>
@@ -52,6 +65,16 @@
           <v-icon left>{{ link.icon }}</v-icon>
           {{ link.title }}
         </v-btn>
+
+        <v-btn
+          v-if="isUserLoggedIn"
+          @click="onLogout"
+          text
+        >
+          <v-icon left>mdi-exit-to-app</v-icon>
+          Logout
+        </v-btn>
+
       </v-toolbar-items>
 
     </v-app-bar>
@@ -86,24 +109,38 @@
 export default {
   data () {
     return {
-      drawer: false,
-      links: [
-        { title: 'Login', icon: 'mdi-lock', url: '/login' },
-        { title: 'Registration', icon: 'mdi-face', url: '/registration' },
-        { title: 'Orders', icon: 'mdi-bookmark-outline', url: '/orders' },
-        { title: 'New ad', icon: 'mdi-file-plus', url: '/new' },
-        { title: 'My ads', icon: 'mdi-format-list-bulleted', url: '/list' }
-      ]
+      drawer: false
     }
   },
   methods: {
     closeError () {
       this.$store.dispatch('clearError')
+    },
+    onLogout () {
+      this.$store.dispatch('logoutUser')
+      this.$router.push('/')
     }
   },
   computed: {
     error () {
       return this.$store.getters.error
+    },
+    isUserLoggedIn () {
+      return this.$store.getters.isUserLoggedIn
+    },
+    links () {
+      if (this.isUserLoggedIn) {
+        return [
+          { title: 'Orders', icon: 'mdi-bookmark-outline', url: '/orders' },
+          { title: 'New ad', icon: 'mdi-file-plus', url: '/new' },
+          { title: 'My ads', icon: 'mdi-format-list-bulleted', url: '/list' }
+        ]
+      }
+
+      return [
+        { title: 'Login', icon: 'mdi-lock', url: '/login' },
+        { title: 'Registration', icon: 'mdi-face', url: '/registration' }
+      ]
     }
   }
 }
--- src/main.js
+++ src/main.js
@@ -28,5 +28,11 @@ new Vue({
 
     // Initialize Firebase
     fb.initializeApp(firebaseConfig)
+
+    fb.auth().onAuthStateChanged(user => {
+      if (user) {
+        this.$store.dispatch('autoLoginUser', user)
+      }
+    })
   }
 }).$mount('#app')
