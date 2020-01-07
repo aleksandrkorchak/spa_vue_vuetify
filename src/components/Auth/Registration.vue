1d65ea8b1b47c8fb304a4569955be900ab9bb166
Alexandr <korchak.alexandr@gmail.com>
2020-01-07 10:24:13 +0200

    Настройка Firebase. Регистрация пользователей

--- src/components/Auth/Registration.vue
+++ src/components/Auth/Registration.vue
@@ -59,7 +59,8 @@
             <v-btn
               color="primary"
               @click="onSubmit"
-              :disabled="!valid"
+              :loading="loading"
+              :disabled="!valid || loading"
             >Create account!</v-btn>
           </v-card-actions>
         </v-card>
@@ -90,6 +91,11 @@ export default {
       ]
     }
   },
+  computed: {
+    loading () {
+      return this.$store.getters.loading
+    }
+  },
   methods: {
     onSubmit () {
       if (this.$refs.form.validate()) {
@@ -97,8 +103,12 @@ export default {
           email: this.email,
           password: this.password
         }
-        console.log(user)
-        console.log(this.$refs.form.value)
+
+        this.$store.dispatch('registerUser', user)
+          .then(() => {
+            this.$router.push('/')
+          })
+          .catch(err => console.log(err))
       }
     }
   }
--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify'
+import * as fb from 'firebase'
 import '@babel/polyfill'
 
 Vue.config.productionTip = false
@@ -11,5 +12,21 @@ new Vue({
   router,
   store,
   vuetify,
-  render: h => h(App)
+  render: h => h(App),
+  created () {
+    // Web app's Firebase configuration
+    var firebaseConfig = {
+      apiKey: 'AIzaSyBIj_wo5X2TSfIve1PUP5C6ihDdTeBP2pQ',
+      authDomain: 'ads-base.firebaseapp.com',
+      databaseURL: 'https://ads-base.firebaseio.com',
+      projectId: 'ads-base',
+      storageBucket: 'ads-base.appspot.com',
+      messagingSenderId: '510774591642',
+      appId: '1:510774591642:web:63f82d23936e10b0a71f23',
+      measurementId: 'G-SCPVGR6QCZ'
+    }
+
+    // Initialize Firebase
+    fb.initializeApp(firebaseConfig)
+  }
 }).$mount('#app')
